ee424913914a3fef4a3a5c1f7d933b03560acdc7
Maria <lompemaria@gmail.com>
2021-12-07 15:07:38 +0100

    Homework

---
+++ homework_python.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+
+#task no 1 
+
+age = 25 
+name = "MARIA LOMPE"
+no = 1.22
+score= "1.0"
+txt="name:{}, age: {}, no: {} homework grade: {}"
+y=score.isdigit()
+print(y)
+z=name.isupper()
+print(z)
+x=txt.title()
+print(x.format(name, age, no, score))
+
+#task no 2 
+
+my_list = [3,4,5,6,7,8,9,10] 
+sum=(my_list[0])+(my_list[7])
+print(sum)
+
+#task no 3 
+
+my_list = [3,4,5,6,7,8,9,10]
+sixth_element=(my_list[5])
+(my_list[5])=int("8")
+print(sixth_element)
+
+#task no 4 
+my_list = [3,4,5,6,7,8,9,10]
+my_list[2], my_list[3] = my_list[3], my_list[2]
+print(my_list)
+
+ 
+
+
+
+
+
+
+
+
+
+    
