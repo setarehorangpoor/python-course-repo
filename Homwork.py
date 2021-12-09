6e7b6095d6038ebbdd5577983eb367fa3528ff69
ahmed-elmeligy7 <80783933+ahmed-elmeligy7@users.noreply.github.com>
2021-12-09 19:42:15 +0100

    Add files via upload

---
+++ Homwork.py
@@ -0,0 +1,17 @@
+my_list = [3, 4, 5, 6, 7, 8, 9, 10]
+
+sum= my_list[0] + my_list[7]
+
+print (sum)
+
+My_list = my_list
+
+string = str(My_list[7])
+
+print (type(string))
+
+My_list1= my_list
+
+my_list[3], my_list[4] = My_list1[4], My_list1[3]
+
+print(My_list1)
